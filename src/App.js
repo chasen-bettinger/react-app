da88345a8c08aa8ff0a2d49065b62c0771153b90
Chasen Bettinger <bettingerchasen@gmail.com>
2017-07-03 18:29:50 -0400

    Synchronizing React UI and State with Controlled Components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state= {
+      todos: [
+        {
+          id: 1,
+          name: 'LearnJSX',
+          isComplete: false
+        },
+        {
+          id: 2,
+          name: 'Build an Awesome App',
+          isComplete: false
+        },
+        {
+          id: 3,
+          name: 'Ship it',
+          isComplete: false
+        }
+      ],
+      currentTodo:''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(evt){
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-App">
+          <form>
+            <input type="text" onChange={this.handleInputChange} value={this.state.currentTodo}/>
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.state.todos.map(todo => <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+                </li> )}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
