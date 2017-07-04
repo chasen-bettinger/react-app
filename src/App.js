0fb6a57a392a4eec09f259a4708fcc24fb5241ae
Chasen Bettinger <bettingerchasen@gmail.com>
2017-07-04 18:20:06 -0400

    Prevent Empty form values

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit= this.handleSubmit.bind(this);
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this);
   }
 
   handleSubmit(evt) {
@@ -39,7 +40,15 @@ class App extends Component {
     const updateTodos = addTodo(this.state.todos, newTodo);
     this.setState({
       todos: updateTodos,
-      currentTodo: ''
+      currentTodo: '',
+      errorMessage:''
+    })
+  }
+
+  handleEmptySubmit(evt) {
+    evt.preventDefault();
+    this.setState({
+      errorMessage: 'Please supply a todo name'
     })
   }
 
@@ -50,6 +59,9 @@ class App extends Component {
   }
 
   render() {
+
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
+
     return (
       <div className="App">
         <div className="App-header">
@@ -57,10 +69,11 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm
              handleInputChange={this.handleInputChange}
              currentTodo={this.state.currentTodo}
-             handleSubmit={this.handleSubmit}/>
+             handleSubmit={submitHandler}/>
 
            <TodoList todos={this.state.todos}/>
 
