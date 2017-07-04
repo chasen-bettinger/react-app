96dc2a22d4b6dc474141f3d826c9df14c7142aac
Chasen Bettinger <bettingerchasen@gmail.com>
2017-07-04 18:48:13 -0400

    update data in a list without mutations

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,16 @@
 export const addTodo = (list, item) => [...list, item]
 
 export const generateId = () => Math.floor(Math.random()*100000);
+
+export const findById = (id, list) => list.find(item => item.id === id)
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete})
+
+export const updateTodos = (list, update) => {
+  const updatedIndex = list.findIndex(item => item.id === update.id);
+  return [
+    ...list.slice(0, updatedIndex),
+    update,
+    ...list.slice(updatedIndex+1)
+  ]
+}
