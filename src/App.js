be1d7c21f511141082eef8cafc73adf0874a62af
Chasen Bettinger <bettingerchasen@gmail.com>
2017-07-05 00:29:50 -0400

    Remove items from a list without mutations

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList} from './components/todo'
-import {addTodo, generateId, findById, toggleTodo, updateTodos} from './lib/todoHelpers'
+import {addTodo, generateId, findById, toggleTodo, updateTodos, removeTodo} from './lib/todoHelpers'
 import {pipe, partial} from './lib/utils'
 
 class App extends Component {
@@ -26,6 +26,12 @@ class App extends Component {
       }
     ],
     currentTodo:''
+  }
+
+  handleRemove = (id, evt) => {
+    evt.preventDefault();
+    const updatedTodos = removeTodo(this.state.todos, id)
+    this.setState({todos: updatedTodos})
   }
 
   handleToggle = (id) => {
@@ -76,7 +82,9 @@ class App extends Component {
              currentTodo={this.state.currentTodo}
              handleSubmit={submitHandler}/>
 
-           <TodoList handleToggle={this.handleToggle} todos={this.state.todos}/>
+           <TodoList handleToggle={this.handleToggle}
+             todos={this.state.todos}
+             handleRemove={this.handleRemove}/>
 
         </div>
       </div>
--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo, findById, toggleTodo, updateTodos} from './todoHelpers'
+import {addTodo, findById, toggleTodo, updateTodos, removeTodo} from './todoHelpers'
 
 test('addTodo should add the passed todo to the list', () => {
   const startTodos = [
@@ -99,4 +99,40 @@ test('updateTodo should not mutate the original array', () => {
   const result = updateTodos(startTodos, updatedTodo)
 
   expect(result).not.toBe(updatedTodo);
+})
+
+
+test('removeTodo should remove an item by id', () => {
+
+  const startTodos = [
+    {id: 1, name: 'one', isComplete:false},
+    {id: 2, name: 'two', isComplete:false},
+    {id: 3, name: 'three', isComplete:false}
+  ]
+
+  const targetId = 2;
+
+  const expectedTodos = [
+    {id: 1, name: 'one', isComplete:false},
+    {id: 3, name: 'three', isComplete:false}
+  ]
+
+  const result = removeTodo(startTodos, targetId);
+
+  expect(result).toEqual(expectedTodos);
+
+})
+
+test('removeTodo should not mutate the original array', () => {
+  const startTodos = [
+    {id: 1, name: 'one', isComplete:false},
+    {id: 2, name: 'two', isComplete:false},
+    {id: 3, name: 'three', isComplete:false}
+  ]
+
+  const targetId = 2;
+
+  const result = removeTodo(startTodos, targetId);
+
+  expect(result).not.toBe(startTodos);
 })
